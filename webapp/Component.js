ec122e1aa3239247472e2564020ac1218ffe509b
Daniel Castaneda <dcastaneda@baycoconsulting.com>
2021-11-04 11:10:12 -0600

    Step 9 - Walkthrough

---
+++ webapp/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function(UIComponent, JSONModel, ResourceModel) {
+    'use strict';
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata : {
+            "interfaces" : ["sap.ui.core.IAsyncContentCreation"],
+            "rootView" : {
+                "viewName" : "sap.ui.demo.walkthrough.view.App",
+                "type" : "XML",
+                "id" : "app"
+            }
+        },
+        init : function() {
+            UIComponent.prototype.init.apply(this, arguments);
+            let oData = {
+                recipient : {
+                    name : "Mundo!"
+                }
+            };
+            let oModel = new JSONModel(oData);
+            this.setModel(oModel);
+            let i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+            });
+            this.setModel(i18nModel, "i18n");
+        }
+    });
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function(Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/m/MessageToast"
+], function(Controller, MessageToast) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onInit : function () {
-            let oData = {
-                recipient : {
-                    name : "Mundo!"
-                }
-            };
-            let oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-            let i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.getView().setModel(i18nModel, "i18n");
-        },
-
         onShowHello : function () {
             let oBundle = this.getView().getModel("i18n").getResourceBundle();
             let sRecipient = this.getView().getModel().getProperty("/recipient/name");
