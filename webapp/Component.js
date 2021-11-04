fb567a38bab5df2225c32a40d3bdd925b5df7851
Daniel Castaneda <dcastaneda@baycoconsulting.com>
2021-11-04 12:11:19 -0600

    Step 10 - Walkthrough

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,12 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function(UIComponent, JSONModel, ResourceModel) {
+    "sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel) {
     'use strict';
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata : {
-            "interfaces" : ["sap.ui.core.IAsyncContentCreation"],
-            "rootView" : {
-                "viewName" : "sap.ui.demo.walkthrough.view.App",
-                "type" : "XML",
-                "id" : "app"
-            }
+            interfaces : ["sap.ui.core.IAsyncContentCreation"],
+            manifest : "json"
         },
         init : function() {
             UIComponent.prototype.init.apply(this, arguments);
@@ -22,10 +17,6 @@ sap.ui.define([
             };
             let oModel = new JSONModel(oData);
             this.setModel(oModel);
-            let i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
         }
     });
 });
